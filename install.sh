8648a70b318d04ff0280c2f56cfc614c0066dd01
KezaBotWa <76220611+KezaBotWa@users.noreply.github.com>
2020-12-22 17:35:02 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,14 @@
+apt update
+apt upgrade
+apt install
+pkg update && pkg upgrade
+pkg install wget
+pkg install ffmpeg
+pkg install nodejs
+pkg install tesseract
+https://github.com/KezaBotWa/Bot-Wa
+cd Bot-Wa
+npm i -g cwebp
+npm i -g ytdl 
+npm i
+npm i got
